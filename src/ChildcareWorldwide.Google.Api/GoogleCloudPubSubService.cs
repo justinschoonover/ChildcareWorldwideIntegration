4236f484e8482f6369f14cf9a044638f9730dc5c
Justin Schoonover <justin.schoonover@gmail.com>
2020-06-17 18:35:23 -0700

    Remove unnecessary usings

--- src/ChildcareWorldwide.Google.Api/GoogleCloudPubSubService.cs
+++ src/ChildcareWorldwide.Google.Api/GoogleCloudPubSubService.cs
@@ -4,7 +4,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using ChildcareWorldwide.Google.Api.Helpers;
 using ChildcareWorldwide.Google.Api.PubSub;
-using Google.Api.Gax;
 using Google.Cloud.PubSub.V1;
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
