3d84259d8e55edac6a3844baa67d3a94fcffb3f6
Justin Schoonover <justin.schoonover@faithlife.com>
2020-11-24 18:03:24 -0900

    Add integration testing for Hubspot property routes

--- test/ChildcareWorldWide.IntegrationTests/Integration/HubspotServiceTestFixture.cs
+++ test/ChildcareWorldWide.IntegrationTests/Integration/HubspotServiceTestFixture.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace ChildcareWorldWide.TestFixtures.Integration
@@ -7,6 +9,8 @@ namespace ChildcareWorldWide.TestFixtures.Integration
 	[Parallelizable(ParallelScope.All)]
 	public sealed class HubspotServiceTestFixture : TestFixtureBase
 	{
+		private const int PropertyTestSampleSize = 10;
+
 		[OneTimeSetUp]
 		public void OneTimeSetup() => OneTimeSetupBase();
 
@@ -37,8 +41,91 @@ namespace ChildcareWorldWide.TestFixtures.Integration
 		public async Task TestGetOptedOutEmailsAsync()
 		{
 			var optedOutEmails = await HubspotService.GetOptedOutEmailsAsync();
-
 			Assert.NotNull(optedOutEmails);
 		}
+
+		[Test]
+		[Category("IntegrationTest")]
+		public async Task TestListContactPropertyGroupsAsync()
+		{
+			var contactPropertyGroups = await HubspotService.ListContactPropertyGroupsAsync().ToListAsync();
+			Assert.NotNull(contactPropertyGroups);
+		}
+
+		[Test]
+		[Category("IntegrationTest")]
+		public async Task TestListContactPropertiesAsync()
+		{
+			var contactProperties = await HubspotService.ListContactPropertiesAsync().ToListAsync();
+			Assert.NotNull(contactProperties);
+		}
+
+		[Test]
+		[Category("IntegrationTest")]
+		public async Task TestListCompanyPropertyGroupsAsync()
+		{
+			var companyPropertyGroups = await HubspotService.ListCompanyPropertyGroupsAsync().ToListAsync();
+			Assert.NotNull(companyPropertyGroups);
+		}
+
+		[Test]
+		[Category("IntegrationTest")]
+		public async Task TestListCompanyPropertiesAsync()
+		{
+			var companyProperties = await HubspotService.ListCompanyPropertiesAsync().ToListAsync();
+			Assert.NotNull(companyProperties);
+		}
+
+		[Test]
+		[TestCaseSource(nameof(GetContactPropertyGroups))]
+		[Category("IntegrationTest")]
+		public async Task TestGetContactPropertyGroupAsync(string groupName)
+		{
+			var contactPropertyGroup = await HubspotService.GetContactPropertyGroupAsync(groupName);
+			Assert.NotNull(contactPropertyGroup);
+			Assert.NotNull(contactPropertyGroup?.Name);
+		}
+
+		[Test]
+		[TestCaseSource(nameof(GetContactProperties))]
+		[Category("IntegrationTest")]
+		public async Task TestGetContactPropertyAsync(string propertyName)
+		{
+			var contactPropertyGroup = await HubspotService.GetContactPropertyAsync(propertyName);
+			Assert.NotNull(contactPropertyGroup);
+			Assert.NotNull(contactPropertyGroup?.Name);
+		}
+
+		[Test]
+		[TestCaseSource(nameof(GetCompanyPropertyGroups))]
+		[Category("IntegrationTest")]
+		public async Task TestGetCompanyPropertyGroupAsync(string groupName)
+		{
+			var companyPropertyGroup = await HubspotService.GetCompanyPropertyGroupAsync(groupName);
+			Assert.NotNull(companyPropertyGroup);
+			Assert.NotNull(companyPropertyGroup?.Name);
+		}
+
+		[Test]
+		[TestCaseSource(nameof(GetCompanyProperties))]
+		[Category("IntegrationTest")]
+		public async Task TestGetCompanyPropertyAsync(string propertyName)
+		{
+			var companyProperty = await HubspotService.GetCompanyPropertyAsync(propertyName);
+			Assert.NotNull(companyProperty);
+			Assert.NotNull(companyProperty?.Name);
+		}
+
+		private static IEnumerable<string> GetContactPropertyGroups() =>
+			GetHubspotService().ListContactPropertyGroupsAsync().ToList().Shuffle().Take(PropertyTestSampleSize).Select(propertyGroup => propertyGroup.Name);
+
+		private static IEnumerable<string> GetContactProperties() =>
+			GetHubspotService().ListContactPropertiesAsync().ToList().Shuffle().Take(PropertyTestSampleSize).Select(propertyGroup => propertyGroup.Name);
+
+		private static IEnumerable<string> GetCompanyPropertyGroups() =>
+			GetHubspotService().ListCompanyPropertyGroupsAsync().ToList().Shuffle().Take(PropertyTestSampleSize).Select(propertyGroup => propertyGroup.Name);
+
+		private static IEnumerable<string> GetCompanyProperties() =>
+			GetHubspotService().ListCompanyPropertiesAsync().ToList().Shuffle().Take(PropertyTestSampleSize).Select(propertyGroup => propertyGroup.Name);
 	}
 }
--- test/ChildcareWorldWide.IntegrationTests/Integration/TestFixtureBase.cs
+++ test/ChildcareWorldWide.IntegrationTests/Integration/TestFixtureBase.cs
@@ -8,8 +8,24 @@ namespace ChildcareWorldWide.TestFixtures.Integration
 {
 	public abstract class TestFixtureBase
 	{
-		protected DrapiService DenariService { get; private set; } = default!;
-		protected HubspotService HubspotService { get; private set; } = default!;
+		private static HubspotService? s_hubspotService;
+
+		protected IDrapiService DenariService { get; private set; } = null!;
+		protected IHubspotService HubspotService { get; private set; } = null!;
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope")]
+		public static HubspotService GetHubspotService()
+		{
+			if (s_hubspotService != null)
+				return s_hubspotService;
+
+			var builder = new ConfigurationBuilder();
+			builder.AddUserSecrets<TestFixtureBase>();
+			builder.AddGoogleSecretsConfiguration();
+			IConfiguration configuration = builder.Build();
+			s_hubspotService = new HubspotService(configuration, new MemoryCache(new MemoryCacheOptions()));
+			return s_hubspotService;
+		}
 
 		[System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope")]
 		public void OneTimeSetupBase()
